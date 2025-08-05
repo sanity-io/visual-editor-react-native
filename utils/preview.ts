af71268fe8fd29fe161b26fa95631160731ccc60
Johnny Povolny <johnny.povolny@sanity.io>
2025-08-05 12:57:59 -0700

    Actually, needed for opening standalone

--- utils/preview.ts
+++ utils/preview.ts
@@ -1,11 +1,19 @@
-import { isMaybePresentation } from "@sanity/presentation-comlink";
+import { SANITY_DATASET, SANITY_PROJECT_ID, SANITY_STUDIO_URL } from '@/constants';
 import { createDataAttribute, CreateDataAttributeProps } from '@sanity/visual-editing';
 import { Platform } from "react-native";
 
 export const isWeb = Platform.OS === 'web'
 
+
+// Your Sanity configuration
+const config = {
+  projectId: SANITY_PROJECT_ID,
+  dataset: SANITY_DATASET,
+  baseUrl: SANITY_STUDIO_URL,
+}
+
 export const createDataAttributeWebOnly = (attr: CreateDataAttributeProps) => {
-  if (isWeb && isMaybePresentation()) {
-    return createDataAttribute(attr)
+  if (isWeb) {
+    return createDataAttribute({...config, ...attr})
   }
 }
