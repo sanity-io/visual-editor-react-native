6d4d12f98a76c8675a9682d2c87783ed81659ea1
Johnny Povolny <johnny.povolny@sanity.io>
2026-07-13 10:20:59 -0700

    Update expo version to 54

--- utils/image_url.ts
+++ utils/image_url.ts
@@ -1,5 +1,5 @@
 import { createImageUrlBuilder } from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import type { SanityImageSource } from '@sanity/image-url'
 import { client } from '../sanity/client'
 
 const builder = createImageUrlBuilder(client)
